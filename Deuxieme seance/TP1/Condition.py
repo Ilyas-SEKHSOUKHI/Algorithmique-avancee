5b59872d5173ae1aa2a95bd6cd6d9f0b7fa4eedb
Ilyas-SEKHSOUKHI <sekhsoukhiilyas@gmail.com>
2025-10-02 12:01:55 +0100

    TP1

---
+++ Deuxieme seance/TP1/Condition.py
@@ -0,0 +1,8 @@
+#Exercice2
+Temp = float(input("Entrez la Temperature"))
+if(Temp<=15):
+    print("Froid")
+elif(Temp>15 and Temp<=25):
+    print("Agreable")
+else:
+    print("Chaud")
---
+++ Deuxieme seance/TP1/EntreesSortiesFstrings.py
@@ -0,0 +1,4 @@
+#Exercice 1
+Nom = input("Entrez Votre Nom")
+age = float(input("Entrez Votre Age"))
+print("Bonjour {} ,vous aurez {} ans l'an prochain.".format(Nom,age+1))
