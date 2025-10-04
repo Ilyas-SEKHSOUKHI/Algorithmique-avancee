37c54bd81b57afbc34725c1ee76ca6f8ac1ca1d7
Ilyas-SEKHSOUKHI <sekhsoukhiilyas@gmail.com>
2025-10-05 00:34:46 +0100

    Ex3 tp1

--- Deuxieme seance/TP1/Listes.py
+++ Deuxieme seance/TP1/Listes.py
@@ -1,1 +1,9 @@
 #Exercice 3
+Stock = ["Produit1","Produit2","Produit3"]
+print(Stock)
+Plus = input("Entrez le produit a ajouter")
+Stock.append(Plus) # Ajouter le produit entrer par user
+print(Stock)
+Sous = input("Entrez le produit a Soustraire")
+Stock.remove(Sous)
+print(Stock)
