18f2c319191085a7937c17adb9f7d06a10bea792
Ilyas-SEKHSOUKHI <sekhsoukhiilyas@gmail.com>
2025-10-05 13:28:18 +0100

    Ex8 py

---
+++ Deuxieme seance/TP1/Sets.py
@@ -0,0 +1,10 @@
+#Exercice 8
+A = {"ilyas@gmail.com","aziz@gmail.com","amira@gmail.com"}
+B = {"Saad@gmail.com","amira@gmail.com","oussama@gmail.com"}
+
+insert = A & B
+union = A | B
+diff = A - B
+print(insert)
+print(union)
+print(diff)
