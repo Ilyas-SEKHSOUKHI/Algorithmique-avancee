459d818a7b691820b871885bc0bb46e3c09a1990
Ilyas-SEKHSOUKHI <sekhsoukhiilyas@gmail.com>
2025-10-16 09:26:36 +0100

    EX4 TP1 2Seance

--- Deuxieme seance/TP1/Slicing.py
+++ Deuxieme seance/TP1/Slicing.py
@@ -1,2 +1,9 @@
 #Exercice 4
-# A Faire
+Code_Commande = "CMD 2025 09 ABC"
+
+milesime = Code_Commande[0:4]
+moi =   Code_Commande[5:7]
+lidentifiant = Code_Commande[8:]
+print("milesime = "+milesime)
+print("moi = "+moi)
+print("l'indentifiant = "+lidentifiant)
