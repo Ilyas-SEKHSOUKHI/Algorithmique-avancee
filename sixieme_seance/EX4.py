b15ae193dab01683ef8cac953ef99f5abf2feda8
Ilyas-SEKHSOUKHI <sekhsoukhiilyas@gmail.com>
2025-10-27 11:10:07 +0100

    .

---
+++ sixieme_seance/EX4.py
@@ -0,0 +1,6 @@
+# Ex4 – Boucle logarithmique
+
+m = n                 # On initialise m avec la valeur de n
+
+while m > 1:          # Tant que m est strictement supérieur à 1
+    m //= 2           # À chaque itération, on divise m par 2 (division entière)
